0d8d795633953e39c111211c13f6394cae915536
Nick Mattei <nmattei89@gmail.com>
2015-08-22 14:49:08 -0600

    almost 1.5

--- app2.js
+++ app2.js
@@ -10,3 +10,15 @@ app.controller('ExercisesController', function ($scope) {
     $scope.rightNow = Date();
     $scope.date = new Date().getTime();
 })
+
+app.controller('directivePractice', function ($scope) {
+  $scope.names = ["Harry", "Ron", "Hermione", "Sirius", "Hedwig", "Tonks"];
+  $scope.symbols = ["&spades;", "&clubs;", "&hearts;", "&diams;"]
+  $scope.numbers = [1,1,2,5,6,9,9,9];
+  $scope.address = {
+    street: '1451 24th Street',
+    city: 'Denver',
+    state: 'CO',
+    zip: 80205
+  }
+})
